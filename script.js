83a49ccbaa684fbbe4dded0426d362f173492522
Jakub Łuckiewicz <jakub.luckiewicz1@gmail.com>
2024-02-17 22:55:32 +0100

    Added some files

---
+++ script.js
@@ -0,0 +1,12 @@
+const tytul1 = document.querySelector('.short-title').innerHTML;
+const tytul2 = document.querySelector('h3>.elprpa--link').innerHTML;
+const tytul3 = document.querySelector('h3>.styel2--link').innerHTML;
+const cena1 = document.querySelector('#jana2v->a>ins').innerHTML;
+const cena2 = document.querySelector('#elprpa->a>ins').innerHTML;
+const cena3 = document.querySelector('#styel2->a>ins').innerHTML;
+console.log('Tytuł: '+tytul1)
+console.log('Cena:' +cena1)
+console.log('Tytuł: '+tytul2)
+console.log('Cena:' +cena2)
+console.log('Tytuł: '+tytul3)
+console.log('Cena:' +cena3)
